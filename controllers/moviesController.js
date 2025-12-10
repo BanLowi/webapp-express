a5993cf3c9119d2acf3c95d91418cfc846c02771
BanLowi <lowi40k@gmail.com>
2025-12-10 15:47:28 +0100

    fix typo on SELECT

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,10 +2,13 @@ const connection = require("../data/connection");
 
 function index(req, res) {
 
-    const sql = "SELCT * FROM movies";
+    const sql = "SELECT * FROM movies";
 
     connection.query(sql, (err, result) => {
-        if (err) return res.status(500).json({ error: true, message: "Database query fail" });
+        if (err) {
+            /* console.log(err); */
+            return res.status(500).json({ error: true, message: "Database query fail" })
+        };
         res.json(result);
     });
 }
